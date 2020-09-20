40c64e4747faec9edbf22dd5cd8f54d77bb6a33b
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2020-09-20 13:29:43 -0300

    project with Interpreter example

---
+++ course/src/main/java/com/course/interpreter/TerminalExpression.java
@@ -0,0 +1,22 @@
+package com.course.interpreter;
+
+import java.util.StringTokenizer;
+
+public class TerminalExpression implements Expression {
+
+	private String data;
+	
+	public TerminalExpression(String data) {
+		this.data = data;
+	}
+	
+	public boolean interpret(String str) {
+		StringTokenizer st = new StringTokenizer(str);
+		while (st.hasMoreElements()) {
+			String test = st.nextToken();
+			if (test.equals(data))
+				return true;
+		}		
+		return false;
+	}
+}
